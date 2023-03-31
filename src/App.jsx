6bba2e908d6dbab9758d274ac68edb5d269d0eb0
TommyNguyen <timsnguyen@hotmail.com>
2023-03-31 06:15:28 -0700

    pushing to pull develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
@@ -8,7 +8,7 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<NotFoundPage />} />
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,30 @@
 import "./NavBar.scss";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function NavBar() {
-  return <div>NAV BAR</div>;
+  return (
+    <div>
+      <div className="nav">NAV BAR</div>
+      <button className="nav-btn" type="button">
+        1
+      </button>
+      <div className="nav-btn__container">
+        <p> text test test</p>
+      </div>
+      <button className="nav-btn" type="button">
+        1
+      </button>
+      <div>
+        <p> text test test</p>
+      </div>
+      <button className="nav-btn" type="button">
+        1
+      </button>
+      <div>
+        <p> text test test</p>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
