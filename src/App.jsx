d0fd24db17cbb124cff4d2122c62c57b6a3e856b
gjpineda <gjpineda@live.com>
2023-03-31 12:05:59 -0700

    added chat components

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import ThankYou from "./pages/ThankYou/ThankYou";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      {/* we need a ternary operator to say if url is /welcome, do not show navbar */}
       <Routes>
-        <Route path="/welcome" element={<ThankYou />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<NotFoundPage />} />
         <Route />
       </Routes>
       <Footer />
