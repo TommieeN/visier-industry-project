59d1c64adf9a1dba1c9c1528bfeba60926db5e42
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-31 05:56:42 -0700

    complete modal opening window

--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -1,14 +1,19 @@
 import "./ThankYou.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function ThankYou() {
+
+    let navigate = useNavigate(); 
+
     return (
         <div className="thankyou">
-            <div className="thankyou__background">
-                <div className="thankyou__modal">
+            <div className="thankyou__background"></div>
+            <div className="thankyou__modal">
                     <h1 className="thankyou__title">Thank you for signing up.</h1>
-                    <button className="thankyou__btn1">Create a report</button>
-                    <button className="thankyou__btn2">Explore</button>
-                </div>
+                    <div className="thankyou__button-box">
+                        <button onClick={()=> navigate('/')} className="thankyou__btn thankyou__btn-1">Create a report</button>
+                        <button onClick={()=> navigate('/')} className="thankyou__btn thankyou__btn-2">Explore</button>
+                    </div>
             </div>
         </div>
     )
