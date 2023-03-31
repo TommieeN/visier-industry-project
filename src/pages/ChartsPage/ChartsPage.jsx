a847d87bb2438353b141b7be2ac2b58c6e126a95
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-31 11:57:21 -0700

    finish landing page and header of charts page

--- src/pages/ChartsPage/ChartsPage.jsx
+++ src/pages/ChartsPage/ChartsPage.jsx
@@ -1,6 +1,9 @@
 import "./ChartsPage.scss";
 import progressImgFive from "../../assets/progress-images/Results.png";
 import sidebar from "../../assets/visier-sidebar.png";
+import download from "../../assets/progress-images/Download-hover.svg";
+import share from "../../assets/progress-images/Share-hover.svg";
+
 
 function ChartsPage() {
 
@@ -13,7 +16,21 @@ function ChartsPage() {
             <div className="main">
               <div className="main__progression">
                 <div className="main__progression-box">
-                <img className="main__progression-img" src={progressImgFive} alt="progression of search query indicator" />
+
+                    <img className="main__progression-img" src={progressImgFive} alt="progression of search query indicator" />
+                    <div className="main__progression-link-container">
+                        <div className="main__progression-linkbox">
+                            <p className="main__progression-text">Download</p>
+                            <img src={download} alt="" className="main__progression-link-img" />
+                        </div>
+
+                        <div className="main__progression-linkbox">
+                            <p className="main__progression-text">Share</p>
+                            <img src={share} alt="" className="main__progression-link-img" />
+                        </div>
+                    </div>
+
+
                 </div>
               </div>
 
