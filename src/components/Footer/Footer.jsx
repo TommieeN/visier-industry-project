8b913afc791b222e0bd4e5b6c376ae36dcac7f20
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-30 20:50:23 -0700

    complete variables and make footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import "./Footer.scss";
+import footerImage from "../../assets/visier-footer-screengrab.png";
 
 function Footer() {
   return (
-    <>
-      <div>FOOTER</div>
-    </>
+    <div className="footer">
+      <img src={footerImage} alt="visier footer screengrab" className="footer-img" />
+    </div>
   );
 }
 
